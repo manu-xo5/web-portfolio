ba1c4bf57cb4df5aeb71ce992bfdb68a71543e54
manu-xo5 <mohit.matwaya@gmail.com>
2025-10-05 07:33:24 +0530

    portfolio v2.0

---
+++ src/site-config.ts
@@ -0,0 +1,29 @@
+export const SiteConfig = {
+	projects: [
+		{
+			label: "Moviera",
+			href: "https://movie-era.netlify.app",
+			description:
+				"Find perfect movie for the evening and watch (trailers) on moviera for free.",
+			releaseDate: new Date(2023, 6, 9),
+		},
+		{
+			label: "The Online Store",
+			href: "https://the-onlinestore.netlify.app",
+			description: "E-commerce platform",
+			releaseDate: new Date(2022, 10, 21),
+		},
+		{
+			label: "Kindo & App",
+			href: "https://www.kintechnology.io",
+			description:
+				"The Ultimate Manager For Your Daily Dosage - Redefine wellness journey with Kindo & App",
+			releaseDate: new Date(2026, 0, 1),
+		},
+	],
+	socials: [
+		{ label: "github", href: "https://github.com/manu-xo5" },
+		{ label: "linkedin", href: "https://www.linkedin.com/in/mohit-matwaya" },
+		{ label: "x.com", href: "https://x.com/mrcherry012" },
+	],
+};
